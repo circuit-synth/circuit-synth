2dfc44c9cc31f15b49bc90adb5d0bf2c98c7fa1d
Shane Mattner <shanemmattner@gmail.com>
2025-08-04 11:28:56 -0700

    add reference project

---
+++ test_single_resistor.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Simple test circuit with a single resistor to debug reference designator issue
+"""
+from circuit_synth import *
+
+@circuit(name='singe_resistor_generated')
+def create_single_resistor():
+    """Test circuit with just one resistor"""
+
+    
+    # Create a single resistor
+    R1 = Component(
+        symbol="Device:R",
+        ref="R",
+        value="10k",
+        footprint="Resistor_SMD:R_0603_1608Metric"
+    )
+    
+
+if __name__ == "__main__":
+    # Generate the circuit
+    circuit_obj = create_single_resistor()
+    
+    # Generate KiCad files
+    circuit_obj.generate_kicad_project(
+        "single_resistor_generated"
+    )
+    
+    print("Generated KiCad project in single_resistor_generated/")
